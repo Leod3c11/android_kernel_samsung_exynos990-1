d620a092a493215484a920edfff1e5dc83e0792e
Leod3c11 <133391551+Leod3c11@users.noreply.github.com>
2025-07-24 16:35:46 -0300

    Update exynos-acme.c
    927mhz gpu
    
    Signed-off-by: Leod3c11 <133391551+Leod3c11@users.noreply.github.com>

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1629,7 +1629,7 @@ static __init int cpufreq_read_cpu_max_c3(char *cpu_max_c3)
 }
 __setup("cpu_max_c3=", cpufreq_read_cpu_max_c3);
 
-unsigned long arg_gpu_max = 897000;
+unsigned long arg_gpu_max = 927000;
 
 static __init int cpufreq_read_gpu_max(char *gpu_max)
 {
