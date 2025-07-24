b0daed84165ad527f274b66825fc7d57754c54b6
Leod3c11 <133391551+Leod3c11@users.noreply.github.com>
2025-07-24 14:55:52 -0300

    Update gpex_clock.c
    Voltagem controle 1
    
    Signed-off-by: Leod3c11 <133391551+Leod3c11@users.noreply.github.com>

--- drivers/gpu/arm/exynos/frontend/gpex_clock.c
+++ drivers/gpu/arm/exynos/frontend/gpex_clock.c
@@ -77,10 +77,7 @@ bool gpex_clock_get_unlock_freqs_status()
 {
 	return clk_info.unlock_freqs;
 }
-/*******************************************
- * static helper functions
- ******************************************/
-static int gpex_clock_update_config_data_from_dt(void)
+int gpex_clock_update_config_data_from_dt(void)
 {
 	int ret = 0;
 	struct freq_volt *fv_array;
@@ -125,7 +122,9 @@ static int gpex_clock_update_config_data_from_dt(void)
 
 	return 0;
 }
-
+/*******************************************
+ * static helper functions
+ ******************************************/
 static int set_clock_using_calapi(int clk)
 {
 	int ret = 0;
