f5bbb3995c77da5892a4f211174c2e40ca5a2292
Leod3c11 <133391551+Leod3c11@users.noreply.github.com>
2025-07-24 22:09:14 -0300

    Add files via upload
    Signed-off-by: Leod3c11 <133391551+Leod3c11@users.noreply.github.com>

---
+++ include/linux/throttle_limit.h
@@ -0,0 +1,36 @@
+/*
+ *  linux/include/linux/throttle_limit.h
+ *
+ */
+
+#ifndef __THROTTLE_LIMIT_H__
+#define __THROTTLE_LIMIT_H__
+
+#ifdef CONFIG_ARM_EXYNOS_ACME
+extern int get_big_throttle_limit(void);
+extern int get_little_throttle_limit(void);
+extern int get_gpu_throttle_limit(void);
+extern bool is_throttle_limit(unsigned int clipped_freq, int cpu);
+#else
+	int get_big_throttle_limit(void)
+{
+	return 0;
+}
+int get_little_throttle_limit(void)
+{
+	return 0;
+}
+int get_gpu_throttle_limit(void)
+{
+	return 0;
+}
+static inline bool is_throttle_limit(unsigned int clipped_freq, int cpu)
+{
+	return false;
+}
+#endif
+
+extern void update_fvmap(int id, int rate, int volt);
+extern void print_fvmap(void);
+
+#endif /* __THROTTLE_LIMIT_H__ */
